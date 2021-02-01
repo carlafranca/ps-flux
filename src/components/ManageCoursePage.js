9aac07935738922e1a6d7b048267c510894d5ee4
Carla Franca <infocarlafranca@gmail.com>
2021-02-01 11:16:36 -0500

    implemented add course form

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = (props) => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: "",
+  });
+
+  function handleChange({ target }) {
+    //create a copy of th ecourse obj (using spread operator) to avoide multating state
+    //using computed property syntax
+    setCourse({
+      ...course,
+      [target.name]: target.value,
+    });
+  }
   return (
     <>
       <h2>Manage Course</h2>
-      {props.match.params.slug}
+      <CourseForm course={course} onChange={handleChange} />
     </>
   );
 };
